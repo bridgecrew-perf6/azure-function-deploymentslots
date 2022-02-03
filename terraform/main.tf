c3e3f06974bcedeedd05b37e22f2bd85eabf11c9
implodingduck <zsek320@gmail.com>
2022-02-02 21:47:34 -0600

    lets clean that

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,28 +53,3 @@ data "azurerm_network_security_group" "basic" {
     resource_group_name = "rg-network-eastus"
 }
 
-module "func1" {
-    source   = "github.com/implodingduck/tfmodules//functionapp"
-    func_name = "fundeploymentslots"
-    resource_group_name = azurerm_resource_group.rg.name
-    resource_group_location = azurerm_resource_group.rg.location
-    working_dir = "../funcactive"
-    app_settings = {
-      "FUNCTIONS_WORKER_RUNTIME" = "node"
-    }
-    linux_fx_version = "node|14"
-    app_identity = [{ 
-      type = "SystemAssigned"
-      identity_ids = null
-    }]
-}
-
-resource "azurerm_function_app_slot" "checkout" {
-  name                       = "checkout"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  app_service_plan_id        = module.func1.asp_id
-  function_app_name          = module.func1.funcion_name
-  storage_account_name       = module.func1.storage_account_name
-  storage_account_access_key = module.func1.storage_account_key
-}
